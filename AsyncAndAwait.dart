6339fc3f87abfa2e2ee599057e930c7b425bd3e0
aadithya-ds <aadithyaprasad.p@datasirpi.com>
2024-02-09 13:47:37 +0530

    Async and await in dart

--- AsyncAndAwait.dart
+++ AsyncAndAwait.dart
@@ -1,18 +1,14 @@
-main() {
-  print("Start");
-  getData();
-  print("End");
+void main() async {
+  print("Start of main function");
+
+  // Using async and await to simulate an asynchronous task (e.g., fetching data)
+  await fetchData();
+
+  print("End of main function");
 }
 
-void getData() async {
-  try {
-    String data = await middleFunction();
-    print(data);
-  } catch (err) {
-    print("Some error $err");
-  }
-}
-
-Future<String> middleFunction() {
-  return Future.delayed(Duration(seconds: 5), () => "Hello");
+Future<void> fetchData() async {
+  print("Fetching data...");
+  await Future.delayed(Duration(seconds: 2)); // Simulate a 2-second delay
+  print("Data fetched!");
 }
